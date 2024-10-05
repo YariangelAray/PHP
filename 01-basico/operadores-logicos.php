027b966f638ebe8db41b6e9d2b0dadb5c5d0d2b0
Yariangel Aray <gabrielamunar4@gmail.com>
2024-10-05 11:52:35 -0500

    php

--- 01-basico/operadores-logicos.php
+++ 01-basico/operadores-logicos.php
@@ -1,1 +1,43 @@
 <?php
+/**
+ * Combinar dos Comparaciones 
+ * 
+ * ---- Simbolo           Nombre ----
+ *       and               and (y)
+ *       or                or (o)
+ *       !                 not (no)
+ *       &&                and (y)
+ *       ||                or (o)
+ * 
+ * 
+ * ------------------ TABLA DEL OPERADOR AND ------------------
+ * Expresión 1               Expresión 2             Resultado
+ *  false           &&        false                   false
+ *  false           &&        true                    false
+ *  true            &&        true                    true
+ *  true            &&        false                   false
+ * 
+ */
+
+ $valor1 = 7;
+ $valor2 = 2;
+
+ var_dump($valor1 = 7 && 2 > 3);
+ echo "<br>";
+ var_dump($valor1 = 7 && 9 > 3);
+ echo "<hr>";
+ 
+/**
+ * ------------------ TABLA DEL OPERADOR OR ------------------
+ * Expresión 1               Expresión 2             Resultado
+ *  false           ||        false                   false
+ *  false           ||        true                    true
+ *  true            ||        false                   true
+ *  true            ||        true                    true
+ */ 
+ 
+ var_dump($valor1 == 7 or 2 > 3);
+ echo "<br>";
+ var_dump($valor1 == 5 || 9 > 3);
+ echo "<br>";
+ var_dump($valor1 == 5 || 1 > 3);
